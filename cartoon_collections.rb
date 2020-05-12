a0b7eaac0a31f432cb7726b8465de1c0cc153908
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-12 20:02:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   
-  array.include?("cheddar" || "gouda" || "camembert")
+   include_it = array.include?("cheddar" || "gouda" || "camembert")
 
 
 end
